bb8d8515e9687f8f6ac41e68e47969be673c5b4e
d-projects <60767346+d-projects@users.noreply.github.com>
2021-05-08 16:50:02 -0700

    Fix static directory issue

--- face_detector/settings.py
+++ face_detector/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,6 +118,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATICFILES_DIR=[
+    os.path.join(BASE_DIR, "static"),
+    'main/static',
+  ]
+
 
 STATIC_URL = '/static/'
 
