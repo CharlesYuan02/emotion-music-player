1068d2c02d9198b22020b379d5fb0f142f362679
d-projects <60767346+d-projects@users.noreply.github.com>
2021-05-08 16:31:21 -0700

    Change endpoints

--- main/views.py
+++ main/views.py
@@ -1,23 +1,13 @@
 from django.shortcuts import render
-from django.http.response import StreamingHttpResponse
-from main.detect import VideoCamera
+from django.http.response import HttpResponse
+#from main.detect import getExpression
 
-def index(response):
-    expression = VideoCamera().get_frame()[1]
-    return render(response, 'index.html', {"exp": expression})
+def index(request):
+    return render(request, 'index.html')
 
-
-def gen(cap):
-    while True:
-        frame = cap.get_frame()[0]
-        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+def getExpression(request):
+    photo = response.POST.get('photo')
+    expression = getExpression(photo)
+    return HttpResponse(expression)
 
 
-def video_feed(response):
-    return StreamingHttpResponse(gen(VideoCamera()), content_type="multipart/x-mixed-replace; boundary=frame")
-
-
-def express(response):
-    while True:
-        expression = cap.get_frame()[1]
-        return render(response, 'templates/index.html', expression=expression)
