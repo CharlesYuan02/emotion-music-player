dcc189a27933e8d84a8d21dff8cdba12dcd58bc1
Charles Yuan <70110720+Chubbyman2@users.noreply.github.com>
2021-05-09 10:49:31 -0400

    Update main.js

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -1,5 +1,7 @@
 let audio, playbtn, nextbtn, prevbtn, mutebtn, title, poster, seekslider, volumeslider, seeking = false, seekto,
-    curtimetext, durtimetext, current_song, dir, playlist, ext, agent, repeat, setvolume;
+    curtimetext, durtimetext, current_song, dir, playlist, ext, agent, repeat, setvolume, angry_playlist, angry_title,
+    angry_poster, happy_playlist, happy_title, happy_poster, calm_playlist, calm_title, calm_poster, sad_playlist,
+    sad_title, sad_poster;
 
 dir = "static/songs/"
 playlist = ["ACDC-BackinBlack", "OhTheLarceny-ManonaMission", "LedZeppelin-ImmigrantSong",
@@ -13,12 +15,27 @@ title = ["ACDC - Back in Black", "Oh The Larceny - Man on a Mission", "Led Zeppe
     "Adele - Hello", "Celine Dion - My Heart Will Go On", "Gary Jules - Mad World"];
 
 poster = ["static/song_imgs/back_in_black.jpg", "static/song_imgs/man_on_a_mission.jpg", "static/song_imgs/immigrant_song.jpg",
-    "static/song_imgs/happy.jpg", "static/song_imgs/celebration.jpg", "static/song_imgs/never_gonna_give_you_up.jpg", "static/song_imgs/all_star.jpg",
+    "static/song_imgs/happy.jpg", "static/song_imgs/celebration.jpg", "static/song_imgs/never_gonna_give_you_up.jpg", "static/song_imgs/all_star.jpeg",
     "static/song_imgs/speed_of_light.jpg", "static/song_imgs/bad_guy.jpg", "static/song_imgs/hello.jpg", "static/song_imgs/my_heart_will_go_on.jpg", "static/song_imgs/mad_world.jpg"];
+
+angry_playlist = ["ACDC-BackinBlack", "OhTheLarceny-ManonaMission", "LedZeppelin-ImmigrantSong"];
+angry_title = ["ACDC - Back in Black", "Oh The Larceny - Man on a Mission", "Led Zeppelin - Immigrant Song"];
+angry_poster = ["static/song_imgs/back_in_black.jpg", "static/song_imgs/man_on_a_mission.jpg", "static/song_imgs/immigrant_song.jpg"];
+
+happy_playlist = ["WillPharrell-Happy", "Kool&TheGang-Celebration", "RickAstley-NeverGonnaGiveYouUp"];
+happy_title = ["static/song_imgs/back_in_black.jpg", "static/song_imgs/man_on_a_mission.jpg", "static/song_imgs/immigrant_song.jpg"];
+happy_poster = ["static/song_imgs/happy.jpg", "static/song_imgs/celebration.jpg", "static/song_imgs/never_gonna_give_you_up.jpg"];
+
+calm_playlist = ["SmashMouth-AllStar", "DJOkawari-SpeedofLight", "BillieEilish-BadGuy"];
+calm_title = ["Smash Mouth - All Star", "DJ Okawari - Speed of Light", "Billie Eilish - Bad Guy"];
+calm_poster = ["static/song_imgs/all_star.jpeg", "static/song_imgs/speed_of_light.jpg", "static/song_imgs/bad_guy.jpg"];
+
+sad_playlist = ["Adele-Hello", "CelineDion-MyHeartWillGoOn", "GaryJules-MadWorld"];
+sad_title = ["Adele - Hello", "Celine Dion - My Heart Will Go On", "Gary Jules - Mad World"];
+sad_poster = ["static/song_imgs/hello.jpg", "static/song_imgs/my_heart_will_go_on.jpg", "static/song_imgs/mad_world.jpg"];
 
 ext = ".mp3";
 agent = navigator.userAgent.toLowerCase()
-
 
 playbtn = document.getElementById("playpausebtn");
 nextbtn = document.getElementById("nextbtn");
@@ -31,17 +48,36 @@ durtimetext = document.getElementById("durtimetext");
 current_song = document.getElementById("current_song");
 repeat = document.getElementById("repeat");
 
-playlist_index = 0;
-
 audio = new Audio();
-audio.src = dir + playlist[0] + ext;
 audio.loop = false;
 
-current_song.innerHTML = title[playlist_index];
+let mood = "Calm";
+switch (mood) {
+    case "Angry":
+        playlist_index = 0;
+        audio.src = dir + angry_playlist[0] + ext;
+        current_song.innerHTML = angry_title[playlist_index];
+        break;
+    case "Happy":
+        playlist_index = 0;
+        audio.src = dir + happy_playlist[0] + ext;
+        current_song.innerHTML = happy_title[playlist_index];
+        break;
+    case "Calm":
+        playlist_index = 0;
+        audio.src = dir + calm_playlist[0] + ext;
+        current_song.innerHTML = calm_title[playlist_index];
+        break;
+    case "Sad":
+        playlist_index = 0;
+        audio.src = dir + sad_playlist[0] + ext;
+        current_song.innerHTML = sad_title[playlist_index];
+        break;
+}
 
 playbtn.addEventListener("click", playPause);
-nextbtn.addEventListener("click", nextSong);
-prevbtn.addEventListener("click", prevSong);
+nextbtn.addEventListener("click", nextSong(mood));
+prevbtn.addEventListener("click", prevSong(mood));
 mutebtn.addEventListener("click", mute);
 seekslider.addEventListener("mousedown", function (event) {
     seeking = true;
@@ -58,18 +94,38 @@ audio.addEventListener("timeupdate", function () {
     seektimeupdate();
 })
 audio.addEventListener("ended", function () {
-    switchTrack();
+    switchTrack(mood);
 })
 repeat.addEventListener("click", loop);
 
 
-function fetchMusicDetails() {
+function fetchMusicDetails(mood) {
     $("#playpausebtn img").attr("src", "static/imgs/pause.png");
-    $("#circle-image img").attr("src", poster[playlist_index]);
+    switch (mood) {
+        case "Angry":
+            $("#circle-image img").attr("src", angry_poster[playlist_index]);
+            current_song.innerHTML = angry_title[playlist_index];
+            audio.src = dir + angry_playlist[playlist_index] + ext;
+            break;
 
-    current_song.innerHTML = title[playlist_index];
+        case "Happy":
+            $("#circle-image img").attr("src", happy_poster[playlist_index]);
+            current_song.innerHTML = happy_title[playlist_index];
+            audio.src = dir + happy_playlist[playlist_index] + ext;
+            break;
 
-    audio.src = dir + playlist[playlist_index] + ext;
+        case "Calm":
+            $("#circle-image img").attr("src", calm_poster[playlist_index]);
+            current_song.innerHTML = calm_title[playlist_index];
+            audio.src = dir + calm_playlist[playlist_index] + ext;
+            break;
+
+        case "Sad":
+            $("#circle-image img").attr("src", sad_poster[playlist_index]);
+            current_song.innerHTML = sad_title[playlist_index];
+            audio.src = dir + sad_playlist[playlist_index] + ext;
+            break;
+    }
     audio.play();
 }
 
@@ -83,20 +139,60 @@ function playPause() {
     }
 }
 
-function nextSong() {
+function nextSong(mood) {
     playlist_index++;
-    if (playlist_index > playlist.length - 1) {
-        playlist_index = 0;
+    console.log(playlist_index);
+    switch (mood) {
+        case "Angry":
+            if (playlist_index > angry_playlist.length - 1) {
+                playlist_index = 0;
+            }
+            break;
+        case "Happy":
+            if (playlist_index > happy_playlist.length - 1) {
+                playlist_index = 0;
+            }
+            break;
+        case "Calm":
+            if (playlist_index > calm_playlist.length - 1) {
+                playlist_index = 0;
+            }
+            break;
+        case "Sad":
+            if (playlist_index > sad_playlist.length - 1) {
+                playlist_index = 0;
+            }
+            break;
     }
-    fetchMusicDetails();
+    fetchMusicDetails(mood);
 }
 
-function prevSong() {
+function prevSong(mood) {
     playlist_index--;
-    if (playlist_index < 0) {
-        playlist_index = playlist.length - 1;
+    console.log(playlist_index);
+    switch (mood) {
+        case "Angry":
+            if (playlist_index < 0) {
+                playlist_index = angry_playlist.length - 1;
+            }
+            break;
+        case "Happy":
+            if (playlist_index < 0) {
+                playlist_index = happy_playlist.length - 1;
+            }
+            break;
+        case "Calm":
+            if (playlist_index < 0) {
+                playlist_index = calm_playlist.length - 1;
+            }
+            break;
+        case "Sad":
+            if (playlist_index < 0) {
+                playlist_index = sad_playlist.length - 1;
+            }
+            break;
     }
-    fetchMusicDetails();
+    fetchMusicDetails(mood);
 }
 
 function mute() {
@@ -153,13 +249,38 @@ function seektimeupdate() {
     }
 }
 
-function switchTrack() {
-    if (playlist_index == playlist.length - 1) {
-        playlist_index = 0;
-    } else {
-        playlist_index++;
+function switchTrack(mood) {
+    switch (mood) {
+        case "Angry":
+            if (playlist_index == angry_playlist.length - 1) {
+                playlist_index = 0;
+            } else {
+                playlist_index++;
+            }
+            break;
+        case "Happy":
+            if (playlist_index == happy_playlist.length - 1) {
+                playlist_index = 0;
+            } else {
+                playlist_index++;
+            }
+            break;
+        case "Calm":
+            if (playlist_index == calm_playlist.length - 1) {
+                playlist_index = 0;
+            } else {
+                playlist_index++;
+            }
+            break;
+        case "Sad":
+            if (playlist_index == sad_playlist.length - 1) {
+                playlist_index = 0;
+            } else {
+                playlist_index++;
+            }
+            break;
     }
-    fetchMusicDetails();
+    fetchMusicDetails(mood);
 }
 
 function loop() {
@@ -170,4 +291,5 @@ function loop() {
         audio.loop = true;
         $("#repeat img").attr("src", "static/imgs/loop1.png");
     }
+}
 }
