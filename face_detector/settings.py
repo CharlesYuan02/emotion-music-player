159ffde1227a59786b026b2cd141b33ab8d00bf9
Charles Yuan <70110720+Chubbyman2@users.noreply.github.com>
2021-05-10 00:36:23 -0400

    Update settings.py

--- face_detector/settings.py
+++ face_detector/settings.py
@@ -133,7 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
